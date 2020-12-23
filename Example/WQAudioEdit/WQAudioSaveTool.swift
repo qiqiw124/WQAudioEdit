4fb289628731f16b71eec30c66c9ef5a1be8f370
01810765 <wangqiqi@smt.cn>
2020-12-23 14:00:59 +0800

    渲染

--- Example/WQAudioEdit/WQAudioSaveTool.swift
+++ Example/WQAudioEdit/WQAudioSaveTool.swift
@@ -35,32 +35,120 @@ class WQAudioSaveTool: NSObject {
             return
         }
         
-        
+        for model:WQAudioEditModel in self.actionTool.editModelArray as! [WQAudioEditModel]{
+
+            let engine = AVAudioEngine()
+            let player = AVAudioPlayerNode()
+            let reverb = AVAudioUnitReverb()
+
+            engine.attach(player)
+            engine.attach(reverb)
+
+            // Set the desired reverb parameters.
+            reverb.loadFactoryPreset(.mediumHall)
+            reverb.wetDryMix = 50
+
+            // Connect the nodes.
+            engine.connect(player, to: reverb, format: model.audioFile.processingFormat)
+            engine.connect(reverb, to: engine.mainMixerNode, format: model.audioFile.processingFormat)
+            
+            player.scheduleSegment(model.audioFile, startingFrame: model.beginFrame, frameCount: AVAudioFrameCount(model.endFrame - model.beginFrame), at: nil, completionHandler: nil)
+
+            do {
+                // The maximum number of frames the engine renders in any single render call.
+                let maxFrames: AVAudioFrameCount = 4096
+                try engine.enableManualRenderingMode(.offline, format: model.audioFile.processingFormat,
+                                                     maximumFrameCount: maxFrames)
+            } catch {
+                fatalError("Enabling manual rendering mode failed: \(error).")
+            }
+
+            do {
+                try engine.start()
+                player.play()
+            } catch {
+                fatalError("Unable to start audio engine: \(error).")
+            }
+
+            // The output buffer to which the engine renders the processed data.
+            let buffer = AVAudioPCMBuffer(pcmFormat: engine.manualRenderingFormat,
+                                          frameCapacity: engine.manualRenderingMaximumFrameCount)!
+
+
+            while engine.manualRenderingSampleTime < model.endFrame - model.beginFrame {
+                do {
+                    let frameCount = model.endFrame - model.beginFrame - engine.manualRenderingSampleTime
+                    let framesToRender = min(AVAudioFrameCount(frameCount), buffer.frameCapacity)
+
+                    let status = try engine.renderOffline(framesToRender, to: buffer)
+
+                    switch status {
+
+                    case .success:
+                        // The data rendered successfully. Write it to the output file.
+                        try writeFile.write(from: buffer)
+
+                    case .insufficientDataFromInputNode:
+                        // Applicable only when using the input node as one of the sources.
+                        break
+
+                    case .cannotDoInCurrentContext:
+                        // The engine couldn't render in the current render call.
+                        // Retry in the next iteration.
+                        break
+
+                    case .error:
+                        // An error occurred while rendering the audio.
+                        fatalError("The manual rendering failed.")
+                    }
+                } catch {
+                    fatalError("The manual rendering failed: \(error).")
+                }
+            }
+
+            // Stop the player node and engine.
+            player.stop()
+            engine.stop()
+            
+            
+        }
         
         
 //
-        self.editPlay.stop()
-        self.editPlay.audioEngine.pause()
-        for model:WQAudioEditModel in self.actionTool.editModelArray as! [WQAudioEditModel]{
-            let buffer:AVAudioPCMBuffer = AVAudioPCMBuffer.init(pcmFormat: model.audioFile.processingFormat, frameCapacity: AVAudioFrameCount(model.endFrame - model.beginFrame))!
-            model.audioFile.framePosition = model.beginFrame
-            do {
-                try model.audioFile.read(into: buffer, frameCount: AVAudioFrameCount(model.endFrame - model.beginFrame))
-            } catch  {
-
-            }
-            do {
-                try writeFile.write(from: buffer)
-            } catch  {
-
-            }
-        }
-        print("\(String(describing: self.fileUrl))")
-        do {
-            try self.editPlay.audioEngine.start()
-        } catch  {
-
-        }
+//        self.editPlay.stop()
+//        self.editPlay.audioEngine.pause()
+//        do {
+//            try self.editPlay.audioEngine.enableManualRenderingMode(.offline, format: self.actionTool.getFormate(), maximumFrameCount: AVAudioFrameCount(self.actionTool.getTotalFrames()))
+//        } catch  {
+//
+//        }
+//
+//        for model:WQAudioEditModel in self.actionTool.editModelArray as! [WQAudioEditModel]{
+//            let buffer:AVAudioPCMBuffer = AVAudioPCMBuffer.init(pcmFormat: model.audioFile.processingFormat, frameCapacity: AVAudioFrameCount(model.endFrame - model.beginFrame))!
+//            model.audioFile.framePosition = model.beginFrame
+//            do {
+//                try model.audioFile.read(into: buffer, frameCount: AVAudioFrameCount(model.endFrame - model.beginFrame))
+//            } catch  {
+//
+//            }
+////            do {
+////                let statu = try self.editPlay.audioEngine.renderOffline(AVAudioFrameCount(model.endFrame - model.beginFrame), to: buffer)
+////                print("\(statu)")
+////            } catch  {
+////
+////            }
+//            do {
+//                try writeFile.write(from: buffer)
+//            } catch  {
+//
+//            }
+//        }
+//        print("\(String(describing: self.fileUrl))")
+//        do {
+//            try self.editPlay.audioEngine.start()
+//        } catch  {
+//
+//        }
         
         
         
